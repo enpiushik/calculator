02e9bc466c8d87ea737a5f33843889bb48092c4d
enpiushik <enpiushik@inbox.lv>
2018-08-23 20:29:36 +0300

    calculate 4 operations

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,50 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        System.out.println(expression[3]);
+        System.out.println(expression[4]);
+        double a = Double.parseDouble(expression[0]);
+        double b = Double.parseDouble(expression[2]);
+
+        double result = 0;
+        switch (expression[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+    }
+    if (expression.length < 4){
+        return String.valueOf(result);
+    }
+    double c = Double.parseDouble(expression[4]);
+    switch (expression[3]) {
+        case "+":
+            result = result + c;
+            break;
+        case "-":
+            result = result - c;
+            break;
+        case "*":
+            result = result * c;
+            break;
+        case "/":
+            result = result / c;
+            break;
+        default:
+            return "ERROR";
+    }
+        return String.valueOf(result);
     }
 
 }
