e0f063dd7ec6c34e18ab0d771db9b64d9930d46b
enpiushik <enpiushik@inbox.lv>
2018-08-23 20:33:55 +0300

    calculate 4 operations

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -11,11 +11,8 @@ class Calculator {
         System.out.println(expression[0]);
         System.out.println(expression[1]);
         System.out.println(expression[2]);
-        System.out.println(expression[3]);
-        System.out.println(expression[4]);
         double a = Double.parseDouble(expression[0]);
         double b = Double.parseDouble(expression[2]);
-
         double result = 0;
         switch (expression[1]) {
             case "+":
@@ -34,6 +31,8 @@ class Calculator {
     if (expression.length < 4){
         return String.valueOf(result);
     }
+    System.out.println(expression[3]);
+    System.out.println(expression[4]);
     double c = Double.parseDouble(expression[4]);
     switch (expression[3]) {
         case "+":
