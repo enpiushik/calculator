2f661b938840ddc262bbdd33c08599fb3c2b4b2c
enpiushik <enpiushik@inbox.lv>
2018-08-28 18:47:44 +0300

    calculator: for (+;-); lesson 28.08.2018

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,65 +8,21 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        double a = Double.parseDouble(expression[0]);
-        double b = Double.parseDouble(expression[2]);
-        double result = 0;
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
-                return "ERROR";
-        }
-        if (expression.length < 4){
-            return String.valueOf(result);
-        }
-        System.out.println(expression[3]);
-        System.out.println(expression[4]);
-        double c = Double.parseDouble(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            case "*":
-                result = result * c;
-                break;
-            case "/":
-                result = result / c;
-                break;
-            default:
-                return "ERROR";
-        }
-        switch (expression[1] + expression[3]){
-            case "+*":
-                result = a + (b * c);
-                break;
-            case "+/":
-                result = a + (b / c);
-                break;
-            case "-*":
-                result = a - (b * c);
-                break;
-            case "-/":
-                result = a - (b / c);
-                break;
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression [i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "ERROR";
+            }
         }
         return String.valueOf(result);
     }
-
 }
