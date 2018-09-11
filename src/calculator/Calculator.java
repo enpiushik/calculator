3026005b9210ee2b80d60a3dddfe416a006ad511
enpiushik <enpiushik@inbox.lv>
2018-09-11 19:12:59 +0300

    calculator: ctrl c + ctrl s TSI104; lesson 11.09.2018

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,26 +12,48 @@ import java.util.List;
 class Calculator {
 
     String calculate(String[] expression) {
-        List<String> tmp = new ArrayList<>();
+        var tmp = new ArrayList<>(List.of(expression));
 
-        double a = Double.parseDouble(expression [0]);
-        for (int i = 1; i < expression.length; i += 2) {
-          String op = expression [i];
-          double b = Double.parseDouble(expression[i + 1]);
-          switch (op){
-              case "+":
-              case "-":
-                  tmp.add(String.valueOf(a));
-                  tmp.add(op);
-                  a = b;
-                  break;
-              case "*":
-                  a *= b;
-                  break;
-              case "/":
-                  a /= b;
-                  break;
-          }
+        int brOpenIdx = -1;
+        do {
+            brOpenIdx = -1;
+            for (int i = 0; i < tmp.size(); i++) {
+                String a = tmp.get(i);
+                if (a.equals("(")) {
+                    brOpenIdx = i;
+                } else if (a.equals(")")) {
+                    var inBr = tmp.subList(brOpenIdx + 1, i);
+                    var result = calculate(inBr);
+                    tmp.subList(brOpenIdx, i + 1).clear();
+                    tmp.add(brOpenIdx, result);
+                    break;
+                }
+            }
+
+        } while (brOpenIdx != -1);
+        return calculate(tmp);
+    }
+
+    String calculate(List<String> expression) {
+        List<String> tmp = new ArrayList<>();
+        double a = Double.parseDouble(expression.get(0));
+        for (int i = 1; i < expression.size(); i+=2) {
+            String op = expression.get(i);
+            double b = Double.parseDouble(expression.get(i + 1));
+            switch (op) {
+                case "+":
+                case "-":
+                    tmp.add(String.valueOf(a));
+                    tmp.add(op);
+                    a = b;
+                    break;
+                case "*":
+                    a *= b;
+                    break;
+                case "/":
+                    a /= b;
+                    break;
+            }
         }
 
         tmp.add(String.valueOf(a));
@@ -52,6 +75,8 @@ class Calculator {
                     return "ERROR";
             }
         }
+
         return String.valueOf(result);
     }
+
 }
